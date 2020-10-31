96b8c5529f0c1482a50810fe2ee6c583088c2f2d
Anastasia <ana.izotova@gmail.com>
2020-10-31 16:34:26 +0300

    add filterByHousingPrice

--- js/filter.js
+++ js/filter.js
@@ -3,10 +3,10 @@
 (() => {
   const filters = document.querySelector(`.map__filters`);
   const housingTypeFilter = filters.querySelector(`#housing-type`);
-  // const housingPriceFilter = filters.querySelector(`#housing-price`);
-  // const roomsNumberFilter = filters.querySelector(`#housing-rooms`);
-  // const guestsNumberFilter = filters.querySelector(`#housing-guests`);
-  // const featuresFilter = filters.querySelector(`#housing-features`);
+  const housingPriceFilter = filters.querySelector(`#housing-price`);
+  const roomsNumberFilter = filters.querySelector(`#housing-rooms`);
+  const guestsNumberFilter = filters.querySelector(`#housing-guests`);
+  const featuresFilter = filters.querySelector(`#housing-features`);
   const ADVERTISEMENTS_AMOUNT = 5;
   const map = document.querySelector(`.map`);
   const pinsContainer = map.querySelector(`.map__pins`);
@@ -48,9 +48,52 @@
     });
   };
 
-  // const filterByHousingPrice = () => {
-  //
-  // };
+  const filterByHousingPrice = () => {
+    const filterValue = housingPriceFilter.value;
+    const data = window.data;
+    let filteredData;
+
+    switch (filterValue) {
+      case `any`:
+        filteredData = data;
+        break;
+      case `middle`:
+        filteredData = data.filter((item) => item.offer.price >= 10000 && item.offer.price <= 50000);
+        break;
+      case `low`:
+        filteredData = data.filter((item) => item.offer.price < 10000);
+        break;
+      case `high`:
+        filteredData = data.filter((item) => item.offer.price > 50000);
+        break;
+    }
+
+    const filteredDataLength = filteredData.length;
+
+    filteredData = filteredDataLength > ADVERTISEMENTS_AMOUNT
+      ? filteredData.slice(0, ADVERTISEMENTS_AMOUNT)
+      : filteredData;
+    const pinsAmount = filteredDataLength > ADVERTISEMENTS_AMOUNT
+      ? ADVERTISEMENTS_AMOUNT
+      : filteredDataLength;
+
+    pinsContainer.querySelectorAll(`.map__pin:not(.map__pin--main)`)
+      .forEach((pin) => pin.remove());
+
+    if (map.querySelector(`.map__card`)) {
+      map.querySelector(`.map__card`).remove();
+    }
+
+    window.pin.makePins(pinsAmount, filteredData);
+    const pins = pinsContainer.querySelectorAll(`.map__pin:not(.map__pin--main)`);
+
+    pins.forEach((pin, index) => {
+      pin.hidden = false;
+      pin.addEventListener(`click`, () => {
+        window.card.openPopup(filteredData[index]);
+      });
+    });
+  };
   //
   // const filterByRoomsNumber = () => {
   //
@@ -66,6 +109,7 @@
 
   const addFilersHandlers = () => {
     housingTypeFilter.addEventListener(`change`, filterByHousingType);
+    housingPriceFilter.addEventListener(`change`, filterByHousingPrice);
   };
 
   window.filter = {
